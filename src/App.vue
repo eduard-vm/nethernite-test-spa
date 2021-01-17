fadff880c0dd2a5a4676cd58937d8d4f8b392116
Eduard Mihalev <ed.mihalev@gmail.com>
2021-01-17 13:22:54 +0300

    Футер и главная

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="app">
-    <router-view/>
+    <main>
+      <router-view />
+    </main>
+    <app-footer />
   </div>
 </template>
 
+<script>
+import AppFooter from '@/components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: { AppFooter },
+};
+</script>
 <style lang="scss">
+html, body {
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+main {
+  min-height: calc(100vh - 120px);
 }
 </style>
